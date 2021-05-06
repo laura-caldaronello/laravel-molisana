f20b5b3e2b860f9a848bd0b0b77a38bd0f27cc01
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-06 15:59:52 +0200

    divisione paste

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,10 +3,24 @@
 @section('titoloPagina','Products')
 
 @section('content')
-    pagina di prodotti
-    @foreach ($pastas as $pasta)
-    <div>
-        {{$pasta['titolo']}}
+    {{-- <div>
+        <h1>le lunghe</h1>
+
     </div>
+    <div>
+        <h1>le corte</h1>
+        
+    </div>
+    <div>
+        <h1>le cortissime</h1>
+        
+    </div> --}}
+    @foreach ($types as $type)
+        <h1>la {{$type}}</h1>
+        @foreach ($pastas as $pasta)
+            @if ($pasta['tipo'] == $type)
+                {{$pasta['titolo']}}
+            @endif
+        @endforeach
     @endforeach
 @endsection
