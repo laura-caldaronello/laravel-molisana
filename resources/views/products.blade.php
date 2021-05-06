8916d1766804976ccff7aee88c92c1adf63dd8ae
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-06 16:29:15 +0200

    sistemate paste

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,24 +3,21 @@
 @section('titoloPagina','Products')
 
 @section('content')
-    {{-- <div>
-        <h1>le lunghe</h1>
-
-    </div>
-    <div>
-        <h1>le corte</h1>
-        
-    </div>
-    <div>
-        <h1>le cortissime</h1>
-        
-    </div> --}}
-    @foreach ($types as $type)
+    <main>
+        @foreach ($types as $type)
         <h1>la {{$type}}</h1>
-        @foreach ($pastas as $pasta)
-            @if ($pasta['tipo'] == $type)
-                {{$pasta['titolo']}}
-            @endif
+        <div class="tipo-container">
+            @foreach ($pastas as $pasta)
+                @if ($pasta['tipo'] == $type)
+                <div class="pasta-container">
+                    <div class="pasta-layover">
+                        <p>{{$pasta['titolo']}}</p>
+                    </div>
+                    <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                </div>
+                @endif
+            @endforeach
+        </div>
         @endforeach
-    @endforeach
+    </main>
 @endsection
