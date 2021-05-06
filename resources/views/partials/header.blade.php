d2824f2839ea75a9ae76082ba4df23fa60b420fb
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-06 14:33:16 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,18 @@
-<div>
-    sono l'header con i seguenti link:
-    <nav>
-        <ul>
-            <li>
-                <a href="/">home</a>
-            </li>
-            <li>
-                <a href="/news">news</a>
-            </li>
-            <li>
-                <a href="/prodotti">prodotti</a>
-            </li>
-        </ul>
-    </nav>
-</div>
+<header>
+    <div class="wrapper">
+        <img src="{{asset('img/logo.png')}}" alt="logo molisana">
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">home</a>
+                </li>
+                <li>
+                    <a href="/news">news</a>
+                </li>
+                <li>
+                    <a href="/prodotti">prodotti</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
