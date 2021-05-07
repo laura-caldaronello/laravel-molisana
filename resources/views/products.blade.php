d4028639798c19a37b53db3e9b1bf245ae51f960
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-07 12:58:22 +0200

    eliminati commenti inutili

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -17,20 +17,5 @@
                 @endforeach
             </div>
         @endforeach
-        {{-- @foreach ($types as $type)
-            <h1>la {{$type}}</h1>
-            <div class="tipo-container">
-                @foreach ($pastas as $k => $pasta)
-                    @if ($pasta['tipo'] == $type)
-                        <div class="pasta-container">
-                            <div class="pasta-layover">
-                                <a href="/prodotti/dettagli/{{$k}}">{{$pasta['titolo']}}</a>
-                            </div>
-                            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-        @endforeach --}}
     </main>
 @endsection
