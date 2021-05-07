a0900028df0b65d7f06f8322fe152ddfd05602b4
laura-caldaronello <caldaronello.l@gmail.com>
2021-05-07 12:55:03 +0200

    route in link per dettagli

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,7 +10,7 @@
                 @foreach ($type as $pasta)
                     <div class="pasta-container">
                         <div class="pasta-layover">
-                            <a href="/prodotti/dettagli/{{$pasta['id']}}">{{$pasta['titolo']}}</a>
+                            <a href="{{route('pagina-dettagli',['id' => $pasta['id']])}}">{{$pasta['titolo']}}</a>
                         </div>
                         <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
                     </div>
